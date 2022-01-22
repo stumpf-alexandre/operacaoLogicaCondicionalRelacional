817e37c465bad9fa893c21bdd47442d1c38f4d59
alsst <alsst@DESKTOP-H1J6UTP>
2022-01-22 17:34:46 -0200

    criando uma operação aninhada

---
+++ operacaoLogicaCondicionalRelacional/src/operacaoLogicaCondicionalRelacional/OperacaoAninhada.java
@@ -0,0 +1,14 @@
+package operacaoLogicaCondicionalRelacional;
+
+public class OperacaoAninhada {
+	public static void main(String[] args) {
+		int nota1 = 60;
+		int nota2 = 49;
+		int nota3 = 80;
+		int nota4 = 70;
+		int media = 0;
+		
+		//calculo da média das notas do aluno
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+	}
+}
