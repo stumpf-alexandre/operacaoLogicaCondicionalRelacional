c1a8b12256ea7f0656d87fe3b146997f4e9cf053
alsst <alsst@DESKTOP-H1J6UTP>
2022-01-22 16:56:17 -0200

    incrementando a função para estudar if e else na classe

--- operacaoLogicaCondicionalRelacional/src/operacaoLogicaCondicionalRelacional/OperacaoIfElse.java
+++ operacaoLogicaCondicionalRelacional/src/operacaoLogicaCondicionalRelacional/OperacaoIfElse.java
@@ -18,5 +18,27 @@ public class OperacaoIfElse {
 		else {
 			System.out.println("Aluno não encontrado");
 		}
+		
+		int nota1 = 90;
+		int nota2 = 60;
+		int nota3 = 80;
+		int nota4 = 70;
+		int media = 0;
+		
+		//calculo da média das notas do aluno
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		//só ira entrar nesta condição se a media for igual ou maior que 70
+		if (media >= 70) {
+			System.out.println("Parabéns, aluno esta aprovado com média: " + media);	
+		}
+		//só ira entrar nesta condição se a media ficar entre 69 e 50 
+		else if (media >= 50 && media <= 69) {
+			System.out.println("Aluno em recuperação, com média: " + media);
+		}
+		//só ira entrar nesta condição caso não tenha entrado em nem uma das outras condições
+		else {
+			System.out.println("Aluno reprovado com média: " + media);
+		}
 	}
 }
