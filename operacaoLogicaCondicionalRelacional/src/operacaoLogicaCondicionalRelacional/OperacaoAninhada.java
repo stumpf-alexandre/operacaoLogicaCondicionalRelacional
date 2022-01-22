4cfdb117535a919f24e06bd5a39aec734defa058
alsst <alsst@DESKTOP-H1J6UTP>
2022-01-22 17:38:39 -0200

    ok

--- operacaoLogicaCondicionalRelacional/src/operacaoLogicaCondicionalRelacional/OperacaoAninhada.java
+++ operacaoLogicaCondicionalRelacional/src/operacaoLogicaCondicionalRelacional/OperacaoAninhada.java
@@ -2,6 +2,7 @@ package operacaoLogicaCondicionalRelacional;
 
 public class OperacaoAninhada {
 	public static void main(String[] args) {
+		//operações lógicas aninhadas são operações dentro de operações
 		int nota1 = 60;
 		int nota2 = 49;
 		int nota3 = 80;
@@ -10,5 +11,23 @@ public class OperacaoAninhada {
 		
 		//calculo da média das notas do aluno
 		media = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		//esta forma de operação não possui media de operações, voce pode aninhar varias operações
+		if (media >= 50) {
+			if (media >= 70) {
+				if (media > 90) {
+					System.out.println("Aluno está aprovado, com média: " + media + ". Parabéns pelo seu esforço");
+				}
+				else {
+					System.out.println("Aluno está aprovado, com média: " + media);
+				}
+			}
+			else {
+				System.out.println("Aluno está em recuperação, com média: " + media);
+			}
+		}
+		else {
+			System.err.println("Aluno está reprovado, com média: " + media);
+		}
 	}
 }
