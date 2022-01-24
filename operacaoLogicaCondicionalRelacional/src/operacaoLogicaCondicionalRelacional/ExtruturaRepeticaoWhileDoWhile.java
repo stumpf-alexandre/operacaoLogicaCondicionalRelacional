71319e53d8a4aa7e367fb8acb4060a42c9b66e2d
alsst <alsst@DESKTOP-H1J6UTP.mshome.net>
2022-01-23 23:37:29 -0200

    incrementando uma extrutura de repetição while

---
+++ operacaoLogicaCondicionalRelacional/src/operacaoLogicaCondicionalRelacional/ExtruturaRepeticaoWhileDoWhile.java
@@ -0,0 +1,13 @@
+package operacaoLogicaCondicionalRelacional;
+
+public class ExtruturaRepeticaoWhileDoWhile {
+	public static void main(String[] args) {
+		int num = 0;
+		
+		//estrutura de repetição while, esta condição executa pelo menos uma vez
+		while (num <= 10) {
+			System.out.println("O número atual é: " + num);
+			num++;
+		}
+	}
+}
