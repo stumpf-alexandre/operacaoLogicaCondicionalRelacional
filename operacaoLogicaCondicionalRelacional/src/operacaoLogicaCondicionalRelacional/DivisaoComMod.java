f0caddee71750234f544b7eb1c35174e96cc6e9d
alsst <alsst@DESKTOP-H1J6UTP.mshome.net>
2022-01-30 08:29:54 -0300

    commit de uma divisão usando o MOD

---
+++ operacaoLogicaCondicionalRelacional/src/operacaoLogicaCondicionalRelacional/DivisaoComMod.java
@@ -0,0 +1,22 @@
+package operacaoLogicaCondicionalRelacional;
+
+public class DivisaoComMod {
+	public static void main(String[] args) {
+		//módulo ou mod é o resto da divisão, é representado por %
+		double carro = 9;
+		double pessoa = 2;
+		
+		double resto = carro % pessoa;
+		double resto2 = carro / pessoa;
+		
+		//aqui transformamos o resultado double em int para tirar o numero com virgula e deixar apenas o numero inteiro
+		int div = (int) resto2;
+		int carr = (int) carro;
+		int pess = (int) pessoa;
+		
+		System.out.println("Foi dividido " + carr + " carros para " + pess + " pessoas");
+		System.out.println("Cada pessoa ficou com " + div + " carro(s)");
+		System.out.println("Sobrou exatamente " + resto + " carro(s).");
+		
+	}
+}
