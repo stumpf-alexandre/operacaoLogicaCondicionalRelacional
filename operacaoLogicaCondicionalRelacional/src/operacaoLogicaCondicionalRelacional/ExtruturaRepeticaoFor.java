ff096e8ff2b77967996999ebcf3739fe1b46a0c5
alsst <alsst@DESKTOP-H1J6UTP.mshome.net>
2022-01-30 07:13:03 -0300

    commit de estrutura de repetição usando o for, junto com o switch case e entrada de dados do teclado

---
+++ operacaoLogicaCondicionalRelacional/src/operacaoLogicaCondicionalRelacional/ExtruturaRepeticaoFor.java
@@ -0,0 +1,44 @@
+package operacaoLogicaCondicionalRelacional;
+
+import java.util.Scanner;
+
+public class ExtruturaRepeticaoFor {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);//instanciando e criando o objeto scaner
+		//extrutura de repetição FOR, usada para percorrer uma quantidade de vezes a condição
+		int num;
+		
+		System.out.println("Digite 1 para imprimir numeros crescentes ou 2 para imprimir numeros decrescentes");
+		num = ler.nextInt();//entrada de dados
+		ler.nextLine();//esvasiando o buffer do teclado
+		
+		switch (num) {
+		case 1:
+			metodoCrescente();
+			break;
+			
+		case 2:
+			metodoDecrescente();
+			break;
+
+		default:
+			System.out.println("Numero errado");
+			break;
+		}
+		
+		
+	}
+	public static void metodoDecrescente() {
+		//neste for o numero que sera imprimido ira de 10 à zero
+		for (int j = 10; j >= 0 ; j--) {
+			System.out.println("O número atual é: " + j);
+		}
+	}
+	private static void metodoCrescente() {
+		//neste for o numero que sera imprimido ira de zero à 10
+		for (int i = 0; i <= 10 ; i++) {
+			System.out.println("O número atual é: " + i);
+		}
+	}
+
+}
